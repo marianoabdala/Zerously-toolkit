f64a4c95e40dcfe92b5d5111d643ea49c9613107
Firstname Lastname <mabdala@gmail.com>
2012-03-11 15:33:56 -0300

    Added a few files.

---
+++ ZYTopAlignedLabel.h
@@ -0,0 +1,13 @@
+//
+//  ZYTopAlignedLabel.h
+//  Zerously Toolkit.
+//
+//  Created by Mariano Abdala on 11/3/11.
+//  Copyright (c) 2011 Zerously.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZYTopAlignedLabel : UILabel
+
+@end
